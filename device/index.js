2751bd140af82df01a84cedb549f1d0d7e4bf225
Moldován Eduárd <eduard@quod.io>
2016-05-25 15:55:40 +0200

    Use MqttClient directly
    Based on https://github.com/aws/aws-iot-device-sdk-js/pull/60

--- device/index.js
+++ device/index.js
@@ -19,6 +19,7 @@ const inherits = require('util').inherits;
 
 //npm deps
 const mqtt = require('mqtt');
+const MqttClient = require('mqtt/lib/client');
 const crypto = require('crypto-js');
 
 //app deps
@@ -190,23 +191,23 @@ function DeviceClient(options) {
    //
    //    +- - - - - - - - - - - - - - - - - - - - - - - - +
    //    |                                                |
-   //                                                      
-   //    |                    FILLING                     |         
-   //                                                      
+   //
+   //    |                    FILLING                     |
+   //
    //    |                                                |
-   //              +-----------------------------+         
+   //              +-----------------------------+
    //    |         |                             |        |
-   //              |                             |         
+   //              |                             |
    //    |         v                             |        |
    //    +- - Established                     Inactive - -+
    //    |         |                             ^        |
-   //              |                             |         
+   //              |                             |
    //    |         |                             |        |
-   //              +----------> Stable ----------+        
+   //              +----------> Stable ----------+
    //    |                                                |
-   //                                                      
-   //    |                     DRAINING                   |         
-   //                                                      
+   //
+   //    |                     DRAINING                   |
+   //
    //    |                                                |
    //    +- - - - - - - - - - - - - - - - - - - - - - - - +
    //
@@ -215,7 +216,7 @@ function DeviceClient(options) {
    //
    // During draining, existing subscriptions are re-sent,
    // followed by any publishes which occurred while offline.
-   //    
+   //
 
    //
    // Operation cache used during filling
@@ -251,23 +252,23 @@ function DeviceClient(options) {
    var drainTimeMs = 250;
 
    //
-   // These properties control the reconnect behavior of the MQTT Client.  If 
-   // the MQTT client becomes disconnected, it will attempt to reconnect after 
+   // These properties control the reconnect behavior of the MQTT Client.  If
+   // the MQTT client becomes disconnected, it will attempt to reconnect after
    // a quiet period; this quiet period doubles with each reconnection attempt,
-   // e.g. 1 seconds, 2 seconds, 2, 8, 16, 32, etc... up until a maximum 
+   // e.g. 1 seconds, 2 seconds, 2, 8, 16, 32, etc... up until a maximum
    // reconnection time is reached.
    //
-   // If a connection is active for the minimum connection time, the quiet 
+   // If a connection is active for the minimum connection time, the quiet
    // period is reset to the initial value.
    //
-   // baseReconnectTime: the time in seconds to wait before the first 
+   // baseReconnectTime: the time in seconds to wait before the first
    //     reconnect attempt
    //
-   // minimumConnectionTime: the time in seconds that a connection must be 
-   //     active before resetting the current reconnection time to the base 
+   // minimumConnectionTime: the time in seconds that a connection must be
+   //     active before resetting the current reconnection time to the base
    //     reconnection time
    //
-   // maximumReconnectTime: the maximum time in seconds to wait between 
+   // maximumReconnectTime: the maximum time in seconds to wait between
    //     reconnect attempts
    //
    // The defaults for these values are:
@@ -455,7 +456,7 @@ function DeviceClient(options) {
 
       //
       // Don't cache subscriptions if auto-resubscribe is disabled
-      // 
+      //
       if (autoResubscribe === false) {
          return;
       }
@@ -477,7 +478,7 @@ function DeviceClient(options) {
    }
 
    //
-   // Return true if the connection is currently in a 'filling' 
+   // Return true if the connection is currently in a 'filling'
    // state
    //
    function _filling() {
@@ -488,7 +489,7 @@ function DeviceClient(options) {
       if (options.protocol === 'wss') {
          var url;
          //
-         // If the access id and secret key are available, prepare the URL. 
+         // If the access id and secret key are available, prepare the URL.
          // Otherwise, set the url to an invalid value.
          //
          if (awsAccessId === '' || awsSecretKey === '') {
@@ -506,13 +507,13 @@ function DeviceClient(options) {
       return protocols[options.protocol](client, options);
    }
 
-   const device = new mqtt.MqttClient(_wrapper, options);
+   const device = new MqttClient(_wrapper, options);
 
    //handle events from the mqtt client
 
    //
    // Timeout expiry function for the connection timer; once a connection
-   // is stable, reset the current reconnection time to the base value. 
+   // is stable, reset the current reconnection time to the base value.
    //
    function _markConnectionStable() {
       currentReconnectTimeMs = baseReconnectTimeMs;
@@ -556,7 +557,7 @@ function DeviceClient(options) {
       // copy of the array.  We shift them out one-by-one until
       // all have been processed, leaving the official record
       // of active subscriptions untouched.
-      // 
+      //
       var subscription = clonedSubscriptions.shift();
 
       if (!isUndefined(subscription)) {
@@ -566,7 +567,7 @@ function DeviceClient(options) {
       } else {
          //
          // Then handle cached operations...
-         // 
+         //
          var operation = offlineOperations.shift();
 
          if (!isUndefined(operation)) {
@@ -584,7 +585,7 @@ function DeviceClient(options) {
          }
          if (offlineOperations.length === 0) {
             //
-            // The subscription and operation queues are fully drained, 
+            // The subscription and operation queues are fully drained,
             // cancel the draining timer.
             //
             clearInterval(drainingTimer);
@@ -607,7 +608,7 @@ function DeviceClient(options) {
       }
       connectionState = 'established';
       //
-      // If not already running, start the draining timer and 
+      // If not already running, start the draining timer and
       // clone the active subscriptions.
       //
       if (drainingTimer === null) {
@@ -641,7 +642,7 @@ function DeviceClient(options) {
       //
       // Update the current reconnect timeout; this will be the
       // next timeout value used if this connect attempt fails.
-      // 
+      //
       currentReconnectTimeMs = currentReconnectTimeMs * 2;
       currentReconnectTimeMs = Math.min(maximumReconnectTimeMs, currentReconnectTimeMs);
       device.options.reconnectPeriod = currentReconnectTimeMs;
@@ -664,7 +665,7 @@ function DeviceClient(options) {
    //
    this.publish = function(topic, message, options, callback) {
       //
-      // If filling or still draining, push this publish operation 
+      // If filling or still draining, push this publish operation
       // into the offline operations queue; otherwise, perform it
       // immediately.
       //
